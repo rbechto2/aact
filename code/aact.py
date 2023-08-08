6680cc226af4be3f67355867d80c96dd0ace1045
Raphael Bechtold <raphaelb@uw.edu>
2023-08-08 16:34:52 -0700

    Added all 3 shapes

--- code/aact.py
+++ code/aact.py
@@ -1,22 +1,32 @@
 import pygame
 import numpy as np
+from utils import *
 
 # Define some colors
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (50, 255, 50)
-RED = (255, 0, 0)
+RED = (255, 50, 50)
 BLUE = (50, 50, 255)
 DKGREEN = (0, 100, 0)
 
 screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 size = screen.get_size()
-print(size)
-shape_size = 100
-square_coords = np.array([size[0]/2 - 1, size[1]/3 - 1]) - np.array([shape_size/2,shape_size/2])
-circle_coords = [size[0]/3 - 1, 2*size[1]/3 - 1]
-hexagon_coords = [2*size[0]/3 - 1, 2*size[1]/3 - 1]
+shape_size = 500
 
+constant_shift = size[1]/20  # Just to make the objects look nicely centered
+# provides offset so center of object is at desired coordinates
+square_center_offset = np.array([shape_size/2, shape_size/2 - constant_shift])
+square_coords = np.array([size[0]/2, size[1]/4]) - square_center_offset
+
+# provides offset so center of object is at desired coordinates
+circle_center_offset = np.array([0, - constant_shift])
+circle_coords = np.array([size[0]/4, 2*size[1]/3]) - circle_center_offset
+
+# provides offset so center of object is at desired coordinates
+hexagon_center_offset = np.array(
+    [-math.sqrt(3)*shape_size/4, (shape_size/4 - constant_shift)])
+hexagon_coords = np.array([3*size[0]/4, 2*size[1]/3]) - hexagon_center_offset
 
 
 active = True
@@ -29,7 +39,11 @@ while active:
             if event.key == pygame.K_ESCAPE:
                 active = False  # Set running to False to end the while loop.
 
-    pygame.draw.circle(screen, BLUE, circle_coords, shape_size)  # DRAW CIRCLE
-    pygame.draw.rect(screen, GREEN, pygame.Rect(square_coords,(shape_size, shape_size)))
+    pygame.draw.circle(screen, BLUE, circle_coords,
+                       shape_size/2)  # DRAW CIRCLE
+    pygame.draw.rect(screen, GREEN, pygame.Rect(
+        square_coords, (shape_size, shape_size)))
+    drawRegularPolygon(
+        screen, RED, hexagon_coords[0], hexagon_coords[1], shape_size/2)
 
     pygame.display.update()
