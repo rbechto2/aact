2a18734ad7121ee789d97affb45cc7325eb37d06
Raphael Bechtold <raphaelb@uw.edu>
2023-10-24 15:39:45 -0700

    fix to call python3 at baylor

--- START_TASK.bat
+++ START_TASK.bat
@@ -1,11 +1,11 @@
 Set "VIRTUAL_ENV=aact_venv"
 
 If Not Exist "%VIRTUAL_ENV%\Scripts\activate.bat" (
-    python.exe -m venv %VIRTUAL_ENV%
+    python3.11 -m venv %VIRTUAL_ENV%
     Call "%VIRTUAL_ENV%\Scripts\activate.bat"
     pip.exe install -r requirements.txt
 ) else (Call "%VIRTUAL_ENV%\Scripts\activate.bat")
 
-python.exe "code\aact.py"
+python3.11 "code\aact.py"
 
 Pause
